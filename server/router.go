8fd1154ad300e164ce8416d6ad85c51195d7ac5c
rastogi-shubham <shu.rastogi337@gmail.com>
2023-02-20 22:43:40 +0530

    feat: added cors middleware and swagger doc

--- server/router.go
+++ server/router.go
@@ -1,13 +1,22 @@
 package server
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"time"
 	"user-service/controllers"
 	"user-service/middlewares"
 )
 
 func NewRouter() *gin.Engine {
 	router := gin.New()
+	router.Use(cors.New(cors.Config{
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization"},
+		AllowCredentials: false,
+		MaxAge:           12 * time.Hour,
+		AllowAllOrigins:  true,
+	}))
 	var uc controllers.IUserController
 	var ac controllers.IAuthController
 	uc = controllers.InitUserController()
@@ -15,9 +24,10 @@ func NewRouter() *gin.Engine {
 
 	root := router.Group("/user-service")
 	{
+		root.Static("/swaggerui/", "cmd/api/swaggerui")
 		ur := root.Group("/user")
-		ur.POST("/upload", middlewares.JwtAuthMiddleware(), uc.UploadUsers)
-		ur.GET("/:id", middlewares.JwtAuthMiddleware(), uc.GetUser)
+		ur.POST("/upload", middlewares.CORSMiddleware(), middlewares.JwtAuthMiddleware(), uc.UploadUsers)
+		ur.GET("/:id", middlewares.CORSMiddleware(), middlewares.JwtAuthMiddleware(), uc.GetUser)
 
 		ar := root.Group("/auth")
 		ar.POST("/register", ac.Register)
